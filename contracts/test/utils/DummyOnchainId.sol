57a435db30f10ba8b586d45766fd38a1d495c581
GianfrancoBazzani <bazzanigianfranco@gmail.com>
2024-12-02 11:32:34 +0100

    :x: chore: delete duplicated files

--- contracts/test/utils/DummyOnchainId.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
-
-// DUMMY CONTRACT USED TO FORCE COMPILATION OF ONCHAINID CONTRACTS
-import {Identity} from "@onchain-id/solidity/contracts/Identity.sol";
-import {ImplementationAuthority} from "@onchain-id/solidity/contracts/proxy/ImplementationAuthority.sol";
-import {IdentityProxy} from "@onchain-id/solidity/contracts/proxy/IdentityProxy.sol";
-import {IdFactory} from "@onchain-id/solidity/contracts/factory/IdFactory.sol";
-import {ClaimIssuer} from "@onchain-id/solidity/contracts/ClaimIssuer.sol";
