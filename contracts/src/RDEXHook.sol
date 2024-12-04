cc4e44c84ba642d5ecef83a6a88d54a50242f462
Sergi Roca Laguna <sergiroca@163.com>
2024-12-04 20:19:27 +0100

    add todos

--- contracts/src/RDEXHook.sol
+++ contracts/src/RDEXHook.sol
@@ -280,6 +280,8 @@ contract RDEXHook is BaseHook, Ownable {
     /// @notice Calculates the fee
     /// @return The calculated fee
     function _calculateFee(address _sender) internal returns (uint24) {
+        //TODO: find way to let user say which topics it wants to be checked for discount during swap
+        //TODO: find way to test that discounts actually get applied
         uint256 discountedFee = BASE_FEE;
 
         for (uint256 i = 0; i < s_topicsWithDiscount.length; i++) {
